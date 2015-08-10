ea81ffbd61c847bae07b8ff277cb72605cd89ca0
Anov Siradj <anov.siradj22@gmail.com>
2015-08-10 22:32:55 +0700

    class config
    semua pengaturan class cain ada disini :)

---
+++ lib/Config.php
@@ -0,0 +1,21 @@
+<?php
+abstract class Config {
+	public static $baseURL = "http://localhost/rantai/";
+	public static $tplDir = "app/template/";
+	public static $tplLayout = "app/layout.php";
+	public static $ctrlDir = "app/controller/";
+	public static $routeSrc = "rantairouting";
+	// public static $routeBaseDefault = "utama";
+	public static $routeBaseAllow = array("utama","portofolio","catatan");
+	// public $mainRoute = array(
+	// 	"utama",
+	// 	"keluarga"
+	// );
+}
+
+function json($v,$t = "Array") {
+	echo $t.": ".json_encode($v)."\n";
+}
+function e($v,$t = "String") {
+	echo $t.": ".$v."\n";
+}
