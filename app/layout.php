001f5b5bcfd4dfc583a81125b4b283ef971a62fd
Anov Siradj <anov.siradj22@gmail.com>
2015-08-10 22:43:17 +0700

    layout

---
+++ app/layout.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <title>this Doc.Title</title> -->
+	<title>Avz: CainController</title>
+	<style type="text/css">
+		#base {
+			width:900px;
+			margin: 0px auto;
+			padding: 0px;
+			position: relative;
+		}
+		pre.box {
+			/*width: 90%;*/
+			/*margin: 10px auto;*/
+			padding: 10px;
+			margin: 20px;
+			border: 2px solid #0099ff;
+		}
+	</style>
+</head>
+<body>
+
+<div id="base">
+<pre class="box">
+<b>source: Layout</b>
+<?php $this->baseView(); ?>
+</pre>
+</div>
+
+</body>
+</html>
