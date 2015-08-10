b71f7f0575f9c650199dc39f48fd30f89db57032
Anov Siradj <anov.siradj22@gmail.com>
2015-08-10 22:44:31 +0700

    utama (or home/index)

---
+++ app/controller/utama.php
@@ -0,0 +1,18 @@
+<?php
+class utama extends cainController {
+	// public static $routeSubAllow = array(1,array("beranda","tentang"));
+	function __construct() {
+		static::$routeSubAllow[1] = array(__CLASS__=>array("beranda","tentang"));
+		static::$routeSubAllow[2] = array("beranda"=>array("profil","keluarga"));
+		// static::$routeSubAllow[2] = array(
+		// 	"beranda"=>array(
+		// 		"profil",
+		// 		"keluarga"
+		// 	)
+		// );
+		// static::$routeSubDefault[1] = "beranda";
+		// if ($this->hasRouteURL(1) && in_array($this->routeURL[1],$this->routeSubAllow[1])) {
+		// 	echo "[yes]";
+		// }
+	}
+}
